04bf1cafa00d5e2b702462a3d20a7693dca7ab69
Cristobal Fuentealba <114311709+Kitobal@users.noreply.github.com>
2022-11-18 18:30:45 -0300

    Update timelinevania.js

--- js/timelinevania.js
+++ js/timelinevania.js
@@ -14,9 +14,9 @@ const timelineData =[
             "title": "Castlevania III: Dracula's Curse",
             "jtitle": "悪魔城伝説 (Akumajō Densetsu)",
             "text": "The genocide of humanity begins at the hand of <b>Count Dracula</b>. Alerted to Dracula's existence, a secret team is sent in by the Eastern Orthodox Church to find and eliminate him. All attempts to suppress Dracula end in failure, so faced with no other options, the Church turns to the Belmont family of vampire hunters in hopes they could destroy Dracula. <b>Trevor Belmont</b> begins his journey into the castle and frees the freedom fighter <b>Grant Danasty</b>, the mage <b>Sypha Belnades</b>, and encounters the son of Dracula, <b>Alucard</b>. Joining forces, they succeed in vanquishing Dracula. Sypha then takes Trevor's hand in marriage, while Alucard, pained from killing his own father, submerges into an eternal slumber.Grant spends the rest of his life rebuilding Wallachia.",
-            "img1": "/img/castlevania/trevor_and_friends.png",
+            "img1": "/img/castlevania/Trevor_and_friends.png",
             "subimg1": "Trevor, Grant, Sypha and Alucard",
-            "img2": "/img/castlevania/dracula_x.png",
+            "img2": "/img/castlevania/Dracula_x.png",
             "subimg2": "Dracula"
         },
         {
@@ -24,9 +24,9 @@ const timelineData =[
             "title": "Castlevania: Curse of Darkness",
             "jtitle": "悪魔城ドラキュラ 闇の呪印 (Akumajō Dracula: Yami no Juin)",
             "text": "Though defeated at the hands of <b>Trevor Belmont</b>, Dracula's curse still ravages the countryside of Europe, bringing with it disease, mob violence and heresy. Amidst this chaos is <b>Hector</b>, a Devil Forgemaster who had formerly worked in the service of <b>Dracula</b>. Yet Hector grew weary and disgusted with the Count's brutal methods and left to live with humans. When his human wife was killed and his fellow forgemaster, <b>Isaac</b>, is implicated in the deed, Hector sets off for Dracula's stronghold to settle the score with him. At the end, it is revealed that Isaac made Hector regain his devil forging powers so that Dracula's Castle would return to the human world. Unbeknownst to Isaac, he himself was being used by <b>Death</b>, Dracula's right-hand-man, as a vessel for his master's reincarnation. Death believed that Hector would kill Isaac and that Dracula's Curse would take control of him, with his body becoming a vessel for Dracula's safe return to Earth. However, Hector realized that his drive for vengeance was caused by Dracula's Curse and resolved that he would no longer be manipulated. Hector did not kill Isaac, which prompted Death to use Isaac's body as an alternative. Hector killed his former master and performed the 'Final Forging' where he summoned the essence of the curse and destroyed it. But the mysterious <b>Saint Germain</b>, whom Hector had met earlier, believed that the war will never end and even when all evil is destroyed, there will still be those who will claim evil's mantle again.",
-            "img1": "/img/castlevania/hector_cod.png",
+            "img1": "/img/castlevania/Hector_cod.png",
             "subimg1": "Hector",
-            "img2": "/img/castlevania/dracula_cod.png",
+            "img2": "/img/castlevania/Dracula_cod.png",
             "subimg2": "Dracula"
         },
         {
@@ -34,9 +34,9 @@ const timelineData =[
           "title": "Castlevania: The Adventure",
           "jtitle": "ドラキュラ伝説 (Dracula Densetsu)",
           "text": "<b>Count Dracula<b/>, slain 100 years before, is reborn. It's rumored the Bartley family was responsible for this act. It's from this point on that the legend begins: 'Once every 100 years, when the faith in God is forgotten, Dracula will come back to life'. <b>Christopher Belmont</b>, descendant of <b>Trevor</b>, apparently succeeds in vanquishing Dracula.",
-          "img1": "/img/castlevania/christopher_belmont.png",
+          "img1": "/img/castlevania/Christopher_Belmont.png",
           "subimg1": "Christopher Belmont",
-          "img2": "/img/castlevania/dracula_x.png",
+          "img2": "/img/castlevania/Dracula_X.png",
           "subimg2": "Dracula"
         },
         {
@@ -44,9 +44,9 @@ const timelineData =[
         "title": "Castlevania II: Belmont's Revenge",
         "jtitle": "ドラキュラ伝説 II (Dracula Densetsu II)",
         "text": "Though <b>Christopher Belmont</b> had defeated the evil <b>Count Dracula</b> fifteen years ago, he did not manage to destroy him, as the count turned himself into mist and managed to escape, building up his energy and biding his time over the years. As Christopher's son came of age, a ceremony was being held to confer the title of Vampire Hunter upon him, so that a father-son team would be formed, but after the ceremony had been completed, Dracula used his powers to take control of <b>Soleil Belmont</b>'s mind and then made four castles appear. Christopher vowed to destroy Dracula and return his son to normal, and in the end, he defeated the evil Count, restored his son and the land was returned to its normal balance once again.",
-        "img1": "/img/castlevania/christopher_belmont.png",
+        "img1": "/img/castlevania/Christopher_Belmont.png",
         "subimg1": "Christopher Belmont",
-        "img2": "/img/castlevania/dracula_x.png",
+        "img2": "/img/castlevania/Dracula_X.png",
         "subimg2": "Dracula"
         },
         {
@@ -54,9 +54,9 @@ const timelineData =[
           "title": "Castlevania",
           "jtitle": "悪魔城ドラキュラ (Akumajō Dracula)",
           "text": "<b>Count Dracula</b> is fully revived by the forces of evil for the second time. <b>Simon Belmont</b> defeats Dracula once more, but in the wake of Dracula's death, is struck with a horrifying curse. With news of his victory spreading around the world, Simon is becoming the most famous Vampire Hunter in history.",
-          "img1": "/img/castlevania/simon_belmont.png",
+          "img1": "/img/castlevania/Simon_Belmont.png",
           "subimg1": "Simon Belmont",
-          "img2": "/img/castlevania/dracula_x.png",
+          "img2": "/img/castlevania/Dracula_X.png",
           "subimg2": "Dracula"
           },
           {
@@ -64,9 +64,9 @@ const timelineData =[
             "title": "Castlevania II: Simon's Quest",
             "jtitle": "ドラキュラⅡ 呪いの封印 (Dracula II: Noroi no Fūin)",
             "text": "The curse inflicted upon <b>Simon</b> seven years earlier continues to affect his body, which slowly starts to decay. A mysterious woman appears to Simon while visiting his family's graveyard and is told that <b>Dracula</b> will be revived when the curse kills him. The only way to lift the curse is for Simon to gather the scattered body parts of Dracula and burn them where his castle once stood. Simon successfully gathers and burns all of the five pieces. However, Dracula manifests as a ghost from a sixth piece. Simon fights valiantly and prevents Dracula's resurrection from taking place.",
-            "img1": "/img/castlevania/simon_belmont.png",
+            "img1": "/img/castlevania/Simon_Belmont.png",
             "subimg1": "Simon Belmont",
-            "img2": "/img/castlevania/dracula_x.png",
+            "img2": "/img/castlevania/Dracula_X.png",
             "subimg2": "Dracula"
           },
           {
@@ -74,9 +74,9 @@ const timelineData =[
             "title": "Castlevania: Harmony of Dissonance",
             "jtitle": "キャッスルヴァニア 白夜の協奏曲 (Castlevania: Byakuya no Concerto)",
             "text": "Fifty years after <b>Simon</b>'s battle, <b>Juste Belmont</b>, grandson of Simon, continues the family's vampire hunting tradition and upholds the duty of searching for Dracula's relics. However, his childhood friend, <b>Lydie</b>, is kidnapped and brought to a mysterious castle, which had suddenly risen up out of the mist. Juste's best friend, <b>Maxim</b>, joins him in the search for Lydie inside the castle. However, it turns out that Maxim was possessed by an evil spirit born out of <b>Dracula</b>'s remains, which were brought together by him. The castle that had appeared was spawned from Maxim's mind. The evil spirit wished to sacrifice Lydie in order to revive the true castle of Dracula. Combining the strength of the three friends, the evil spirit is defeated, his hold on Maxim disappears and the castle vanishes back into the mist.",
-            "img1": "/img/castlevania/juste_belmont.png",
+            "img1": "/img/castlevania/Juste_Belmont.png",
             "subimg1": "Juste Belmont",
-            "img2": "/img/castlevania/dracula_hod.png",
+            "img2": "/img/castlevania/Dracula_hod.png",
             "subimg2": "Dracula Wraith"
           },
           {
@@ -84,9 +84,9 @@ const timelineData =[
             "title": "Castlevania: Rondo of Blood",
             "jtitle": "悪魔城ドラキュラＸ ～血の輪廻～ (Akumajō Dracula X: Chi no Rondo)",
             "text": "Called back to life by the Dark Priest <b>Shaft</b>, <b>Dracula</b> is awakened again. Richter Belmont, descendant of <b>Simon</b> and <b>Juste</b>, sets out to find and kill Dracula and save the women kidnapped from his town, including his fiancée <b>Annette</b>. Aided by <b>Maria Renard</b>, a girl with mysterious powers, Richter prevails over Dracula. However, Richter was left defenseless from his battle with Dracula and Shaft's curse enters his body.",
-            "img1": "/img/castlevania/richter_rob.png",
+            "img1": "/img/castlevania/Richter_rob.png",
             "subimg1": "Richter Belmont",
-            "img2": "/img/castlevania/dracula_rob.png",
+            "img2": "/img/castlevania/Dracula_rob.png",
             "subimg2": "Dracula"
           },
           {
@@ -94,9 +94,9 @@ const timelineData =[
             "title": "Castlevania: Symphony of the Night",
             "jtitle": "悪魔城ドラキュラＸ月下の夜想曲 (Akumajō Dracula X: Gekka no Yasōkyoku)",
             "text": "<b>Shaft</b> would continue to exist as a spirit contained within a crystal ball. However, in the five years since the previous battle, Shaft manages to regain power through the curse placed on <b>Richter</b>. After four years, Shaft succeeds in gaining full control over Richter. He plans to revive Dracula with the aid of Richter's powers. Maria sets out to find the missing Richter only to stumble on <b>Dracula</b>'s castle which had risen out of the mist. Sensing the rise of evil, <b>Alucard</b> awakes from a 300 year slumber. Thanks to Alucard, Richter is freed from Shaft's influence. Unable to influence Richter further but with enough power to act on his own, Shaft summons Dracula's true castle as an inverted floating version in the sky above the current castle. Shaft lets Alucard collect the pieces of Dracula's body and then uses his own spirit as fuel to summon Dracula himself with them. Alucard battles his father and eventually defeats him as he is using an imperfect form. Maria, Richter and Alucard meet outside as the castle crumbles. Alucard leaves to return to his sleep and Maria decides to leave with him, as she has fallen in love with him. Sometime later, Richter vanishes from history.",
-            "img1": "/img/castlevania/alucard_sotn.png",
+            "img1": "/img/castlevania/Alucard_sotn.png",
             "subimg1": "Alucard",
-            "img2": "/img/castlevania/dracula_sotn.png",
+            "img2": "/img/castlevania/Dracula_sotn.png",
             "subimg2": "Dracula"
           },
           {
@@ -104,9 +104,9 @@ const timelineData =[
             "title": "Castlevania: Order of Ecclesia",
             "jtitle": "悪魔城ドラキュラ 奪われた刻印 (Akumajō Dracula: Ubawareta Kokuin)",
             "text": "<b>Shanoa</b>, an Ecclesia member, has been chosen to be the bearer of Dominus, a Glyph that was developed to be used as a weapon against <b>Dracula</b>. Fellow member <b>Albus</b> steals the three pieces that make up Dominus out of jealousy. Shanoa pursues Albus and manages to retrieve the pieces. When she returns to Ecclesia's leader, <b>Barlowe</b>, it's revealed he wishes to revive Dracula with the help of Dominus. He and Shanoa battle, but Dracula's spirit leaks from Dracula's remains, which were stored in Ecclesia's headquarters, and takes control of Barlowe, resulting in Dracula being reborn in a host body. Shanoa heads to Dracula's newly risen castle and confronts him, managing to defeat him in the end.",
-            "img1": "/img/castlevania/shanoa_ooe.png",
+            "img1": "/img/castlevania/Shanoa_ooe.png",
             "subimg1": "Shanoa",
-            "img2": "/img/castlevania/dracula_ooe.png",
+            "img2": "/img/castlevania/Dracula_ooe.png",
             "subimg2": "Dracula"
           },
           {
@@ -114,9 +114,9 @@ const timelineData =[
             "title": "Castlevania: Bloodlines",
             "jtitle": "バンパイアキラー (Vampire Killer)",
             "text": "The legendary vampire <b>Elizabeth Bartley</b>, Dracula's niece, suddenly appears in the 20th century. Plotting a sacrificial war in order to bring back her uncle back to life, she orchestrates the beginning of World War I. <b>Quincy Morris</b>' son, <b>John Morris</b>, and his best friend <b>Eric Lecarde</b>, vow to take up the fight against evil. Together they manage to prevent Dracula's full resurrection.",
-            "img1": "/img/castlevania/john_and_eric.png",
+            "img1": "/img/castlevania/John_and_Eric.png",
             "subimg1": "John Morris and Eric Lecarde",
-            "img2": "/img/castlevania/elizabeth_bartley.png",
+            "img2": "/img/castlevania/Elizabeth_bartley.png",
             "subimg2": "Elizabeth Bartley"
           },
           {
@@ -124,9 +124,9 @@ const timelineData =[
             "title": "Castlevania: Portrait of Ruin",
             "jtitle": "悪魔城ドラキュラ ギャラリー オブ ラビリンス (Akumajō Dracula: Gallery of Labyrinth)",
             "text": "The world is in chaos due to the second world war. The souls of the dead roam the earth. Their anguish summons <b>Dracula</b>'s legendary castle. <b>Jonathan Morris</b>, son of <b>John</b>, and his ally <b>Charlotte Aulin</b>, step forward to fight evil. They discover a vampire named <b>Brauner</b>, who has claimed the castle as his own. They fight him in order to destroy the castle, but by defeating Brauner, Dracula is freed from the seal that kept him locked away. The Count is defeated once more.",
-            "img1": "/img/castlevania/jonathan_charlotte.png",
+            "img1": "/img/castlevania/Jonathan_charlotte.png",
             "subimg1": "Jonathan and Charlotte",
-            "img2": "/img/castlevania/dracula_por.png",
+            "img2": "/img/castlevania/Dracula_por.png",
             "subimg2": "Dracula"
           },
           {
@@ -134,9 +134,9 @@ const timelineData =[
             "title": "Castlevania: Aria of Sorrow",
             "jtitle": "キャッスルヴァニア ～暁月の円舞曲～ (Castlevania: Akatsuki no Minuet)",
             "text": "It's prophesied that the supposedly perished <b>Dracula</b> would return again. In Japan, in the year 2035 on the night of a solar eclipse, high school students <b>Soma Cruz</b> and <b>Mina Hakuba</b> are transported to Dracula's castle when entering the Hakuba Shrine. When Soma awakes, he finds himself within the confines of the castle. In order to escape the castle, he teams up with <b>Genya Arikado</b>, <b>Yoko Belnades</b> and the amnesic 'J' to defeat <b>Graham Jones</b>, who believes he is the new Dracula. When he finally arrives at the throne room, he learns he himself is Dracula reborn after he defeats Graham. To free himself from the chaotic power of Dracula, he has to travel to the chaos realm on the other 'side' of Dracula's castle. After managing to liberate himself from Dracula's power and destroying the source of chaos, he escapes the castle together with his friends.",
-            "img1": "/img/castlevania/soma_aos.png",
+            "img1": "/img/castlevania/Soma_aos.png",
             "subimg1": "Soma Cruz",
-            "img2": "/img/castlevania/graham.png",
+            "img2": "/img/castlevania/Graham.png",
             "subimg2": "Graham Jones"
           },
           {
@@ -144,9 +144,9 @@ const timelineData =[
             "title": "Castlevania: Dawn of Sorrow",
             "jtitle": "悪魔城ドラキュラ 蒼月の十字架 (Akumajō Dracula: Sōgetsu no Jūjika)",
             "text": "It has been one year since the uproar about <b>Dracula</b> in the eclipse. <b>Soma Cruz</b> is at peace. But a cult leader, <b>Celia Fortner</b>, attacked Soma in broad daylight and tells him to follow her back to Dracula's castle. In order to avoid involving Mina, Soma enters the castle alone. <b>Yoko Belnades</b>, <b>Julius Belmont</b> and <b>Genya Arikado</b> also arrive with the intention of stopping the cult. With Soma's arrival in the castle, Celia begins her plan for the advent of the new Dark Lord. She tries to instigate the revival of Dracula by having Soma embrace his darkness or, if that would fail, having another heir of Dracula's power kill him. Celia fails to get Soma to turn, but is killed later by another candidate, <b>Dmitrii Blinov</b>, who copied Soma's power of Dominance. Yoko, Julius and Arikado help Soma track him down but they are too late. Trying to acquire souls like Soma, he loses control and transforms into a giant monster called Menace, who is comprised of all the souls he collected. After its defeat, Soma returns home.",
-            "img1": "/img/castlevania/soma_dos.png",
+            "img1": "/img/castlevania/Soma_dos.png",
             "subimg1": "Soma Cruz",
-            "img2": "/img/castlevania/celia_fortner.png",
+            "img2": "/img/castlevania/Celia_Fortner.png",
             "subimg2": "Celia Fortner"
           }
             
